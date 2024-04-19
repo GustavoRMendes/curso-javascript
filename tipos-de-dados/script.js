36ea4afa2eea5f5abe763746c3d21b035f0d589d
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-19 11:09:33 -0400

    resolver exercicio proposto sobre tipo de dados

--- tipos-de-dados/script.js
+++ tipos-de-dados/script.js
@@ -16,10 +16,22 @@ console.log(typeof sobrenome);
 // Concatenar
 const timeFavorito = "Barcelona";
 const rival = "Real Madrid";
-console.log('o ' + timeFavorito + ' é rival do ' + rival);
+console.log("o " + timeFavorito + " é rival do " + rival);
 
 // Template String
 const anoDeNascimento = 2004;
-const mesDeNascimento = 'Outubro';
+const mesDeNascimento = "Outubro";
 const diaDoNascimento = 20;
-console.log(`Eu nasci dia ${diaDoNascimento} no mês de ${mesDeNascimento} no ano de ${anoDeNascimento}!`)
+console.log(
+  `Eu nasci dia ${diaDoNascimento} no mês de ${mesDeNascimento} no ano de ${anoDeNascimento}!`
+);
+
+// Exercício
+const animal = "Cachorro";
+const numeroTexto = "2004";
+const minhaIdade = 19;
+const meuNome = "Gustavo";
+const meuSobrenome = "Mendes";
+console.log("Meu nome completo é " + meuNome + " " + meuSobrenome);
+const frase = "It ' s time";
+console.log(typeof meuNome);
