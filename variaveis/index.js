1fb4311a0d0cb5a6f204b4bb547e9a744062351c
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-19 10:25:33 -0400

    criar conteudo de aprendizado sobre as variaveis

---
+++ variaveis/index.js
@@ -0,0 +1,19 @@
+// Variáveis
+// var, let ou const.
+
+let nome = 'Gustavo';
+console.log(nome);
+
+const preco = 25;
+const totalComprado = 5;
+const totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+let sobrenome = 'Mendes', 
+  cidade = 'Porto Velho';
+console.log(sobrenome);
+console.log(cidade); 
+
+let aplicativoIndefinido;
+console.log(aplicativoIndefinido);
+
