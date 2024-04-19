55ed8212d16d230e25c2010d12be00db12bbd688
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-19 11:29:41 -0400

    adicionando a primeira parte do conteudo sobre numeros e operadores

--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -3,3 +3,17 @@ const idade = 19;
 const gols = 1000;
 const pi = 3.14;
 const exp = 2e10; 
+
+// Operadores Aritméticos
+const soma = 1 + 1;
+const subtracao = 2 - 2;
+const multiplicacao = 10 * 10;
+const divisao = 10 / 2;
+const expoente = 2 ** 4;
+const modulo = 14 % 5;
+
+// Operadores Aritméticos (String)
+const somaString = "100" + 50; // 10050
+const subtracaoString = 100 - "50"; // 50
+const multiplicacaoString = "100" * "2"; //200
+const divisaoString = 'Comprei 10' / 2; // NaN
