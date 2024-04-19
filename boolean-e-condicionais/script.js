9b267380ead07ae9ea5af3129accc3a2bdfd5d1f
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-19 16:35:08 -0400

    adicionar truthy e falsy

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -1,9 +1,18 @@
+// Boolean e Condicional
 const possuiGraducao = false;
 const possuiDoutorado = true;
 if (possuiGraducao) {
   console.log("Possui Graduação");
-} else if(possuiDoutorado) {
+} else if (possuiDoutorado) {
   console.log("Possui doutorado");
 } else {
-  console.log("Não possui nada.")
+  console.log("Não possui nada.");
+}
+
+const nome = "Gustavo";
+// Truthy e Falsy
+if (nome) {
+  console.log(nome);
+} else {
+  console.log("nome não existe");
 }
