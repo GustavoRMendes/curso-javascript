fb43fb2d4dbaadbee88ae21c310ee678a20b316a
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-19 11:00:32 -0400

    adicionar conteudo aprendido dos tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,25 @@
+// Tipos de Dados
+
+const nome = "Gustavo"; // string
+const numero = 11; // number
+const possuiTrabalho = false; // boolean
+let time; // undefined
+let comida = null; // null
+let simbolo = Symbol(); // Symbol
+const person = {}; // Object
+
+// Verificar tipo de dados
+// typeof
+const sobrenome = "Mendes";
+console.log(typeof sobrenome);
+
+// Concatenar
+const timeFavorito = "Barcelona";
+const rival = "Real Madrid";
+console.log('o ' + timeFavorito + ' é rival do ' + rival);
+
+// Template String
+const anoDeNascimento = 2004;
+const mesDeNascimento = 'Outubro';
+const diaDoNascimento = 20;
+console.log(`Eu nasci dia ${diaDoNascimento} no mês de ${mesDeNascimento} no ano de ${anoDeNascimento}!`)
