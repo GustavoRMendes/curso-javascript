aba0439c73a86ad346e16cff0d5b1715b50a63ee
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-26 11:48:47 -0400

    adicionar exercício resolvido

--- funcoes/script.js
+++ funcoes/script.js
@@ -41,3 +41,69 @@ function mostrarAlerta(mensagem) {
   }
 }
 addEventListener("click", mostrarAlerta("string"));
+
+// Exercício
+// Crie uma função para verificar se um valor é Truthy
+function verification(value) {
+  if (value) {
+    console.log("verdade")
+  } else {
+    console.log("false")
+  }
+}
+// Crie uma função matemática que retorne o perímetro de um quadrado
+function perimetroQuadrado(lado1,lado2,lado3,lado4){
+  return lado1 + lado2 + lado3 + lado4
+}
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function showFullname(nome,sobrenome) {
+  return `Meu nome completo é ${nome} ${sobrenome}`
+}
+// Crie uma função que verifica se um número é par
+function verificarNumero(numero) {
+  if (numero % 2 === 0) {
+    return "Par"
+  } else {
+    return "Ímpar"
+  }
+}
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function tipoDeDado(valor) {
+  if (typeof valor === "string") {
+    return "String"
+  } else if (typeof valor === "number") {
+    return "Number"
+  } else if (typeof valor === "object") {
+    return "Object"
+  } else if (typeof valor === "boolean") {
+    return "Boolean"
+  } else if (typeof valor === "function") {
+    return "Function"
+  } else if (typeof valor === "symbol") {
+    return "Symbol"
+  } else if(typeof valor === "undefined") {
+    return "Undefined"
+  } else {
+    console.log("tente novamente")
+  }
+}
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+function showName(nome){
+  console.log(nome)
+}
+addEventListener("scroll", showName("gustavo"))
+// Corrija o erro abaixo
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+console.log(precisoVisitar(20))
+console.log(jaVisitei(20));
