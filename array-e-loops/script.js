5a8264e861f2a01b1451f0bd2de280594530d8d2
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-30 17:59:18 -0400

    adicionar conteúdo sobre for loop e array

---
+++ array-e-loops/script.js
@@ -0,0 +1,13 @@
+const lista = ['Banana','Maça','Pera'];
+console.log(lista);
+
+lista.pop();
+console.log(lista);
+
+for(let i = 0; i < lista.length; i++) {
+  console.log(lista[i]);
+}
+
+for (let numero = 0; numero <= 10; numero++ ){
+  console.log(numero);
+}
