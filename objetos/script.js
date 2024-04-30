75ddf906d0d79287843068d1314d767eb9ae58a5
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-30 10:31:03 -0400

    adicionar o conteúdo dos objetos

---
+++ objetos/script.js
@@ -0,0 +1,22 @@
+const person = {
+  firstName: "Gustavo",
+  age: 19,
+};
+
+console.log(person);
+console.log(person.age);
+console.log(person.firstName);
+
+const quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+};
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(2));
+console.log(Math.random());
+console.log(Math.PI);
