299288d9504b838d1f7fd4943aceb39795614d79
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-30 14:57:26 -0400

    adicionar exercício sobre métodos e propriedades de strings e do DOM

--- tudo-e-objeto/script.js
+++ tudo-e-objeto/script.js
@@ -1,14 +1,30 @@
 const nome = "Gustavo";
-console.log(nome.replace("stavo",""));
+console.log(nome.replace("stavo", ""));
 console.log(nome.toLowerCase());
 
-const numero  = 1.44432;
+const numero = 1.44432;
 console.log(numero.toFixed(2));
 
 console.log(addEventListener.length);
 
-
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
-  alert("Hello, World!")
-})
+  alert("Hello, World!");
+});
+
+// Exercício
+const texto = "Olá, Mundo!";
+console.log(texto.length);
+console.log(texto.toLowerCase());
+console.log(texto.toUpperCase());
+console.log(replace("Olá", "Oie"));
+
+const paragraph = document.querySelector(".paragraph");
+// paragraph.classList
+// paragraph.addEventListener
+// paragraph.append
+// paragraph.contains
+// paragraph.innerHTML
+// paragraph.setAttribute
+// paragraph.prepend
+// paragraph.remove
