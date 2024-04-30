75d6ae050d1428cd4c8f18afdf54cab4f1eb54f6
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-30 10:47:00 -0400

    adicionar exercício resolvido sobre objetos

--- objetos/script.js
+++ objetos/script.js
@@ -21,3 +21,33 @@ console.log(quadrado.perimetro(2));
 console.log(Math.random());
 console.log(Math.PI);
 console.log(quadrado.hasOwnProperty("lados")); //true
+
+// Exercício
+
+const pessoa = {
+  nome: "Gustavo",
+  sobrenome: "Mendes",
+  mostrarNome() {
+    return this.nome + " " + this.sobrenome;
+  },
+};
+console.log(pessoa.mostrarNome());
+const carro = {
+  preco: 1000,
+  portas: 4,
+  marca: "Audi",
+};
+carro.preco = 3000;
+const cachorro = {
+  raca: "Labrador",
+  cor: "preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === "homem") {
+      return "Latiu";
+    } else {
+      return "Nada";
+    }
+  },
+};
+console.log(cachorro.latir("homem"));
