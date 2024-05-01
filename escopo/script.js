562820de7e042f9c48464c50946274b7c502096d
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-01 11:20:46 -0400

    adicionar função e use strict

---
+++ escopo/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Escopo de Função
+function mostrarCarro() {
+  let carro = "Fusca";
+  console.log(carro);
+}
+mostrarCarro(); // Fusca
+// console.log(carro); carro is not defined;
