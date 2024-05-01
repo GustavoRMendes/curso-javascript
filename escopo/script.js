f3f35ea239e46d7814034d702d4967cb91687162
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-01 11:51:47 -0400

    adiiconar exercíico sobre escopo

--- escopo/script.js
+++ escopo/script.js
@@ -18,4 +18,32 @@ if (true) {
 {
   const carro2 = "Ferrari"
 }
-console.log(carro2); // Not defined
+// console.log(carro2); // Not defined
+
+// Exercício
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+}
+console.log(cor);
+
+ //Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+// O que fazer para total retornar 500?
+const numero = 50;
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+const total = 10 * numero;
+console.log(total);
