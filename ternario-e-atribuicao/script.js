03f43779a6db0f4a7d626bdd969fe6371603daac
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-01 10:27:33 -0400

    adicionar conteúdo sobre os operadores de atribuição e ternário

---
+++ ternario-e-atribuicao/script.js
@@ -0,0 +1,9 @@
+// Operadores de Atribuição
+let numero = 20;
+numero += 10;
+console.log(numero);  
+
+// Ternário
+const idade = 18;
+const resultado = (idade >= 18) ? "Maior de idade" : "Menor de Idade";
+console.log(resultado);
