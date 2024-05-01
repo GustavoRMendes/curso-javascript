d9055cc428433f9df0a83ea28e26ab7e224241df
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-01 10:29:42 -0400

    adicionar conteúdo sobre o if abreviado

--- ternario-e-atribuicao/script.js
+++ ternario-e-atribuicao/script.js
@@ -1,9 +1,15 @@
 // Operadores de Atribuição
 let numero = 20;
 numero += 10;
-console.log(numero);  
+console.log(numero);
 
 // Ternário
 const idade = 18;
-const resultado = (idade >= 18) ? "Maior de idade" : "Menor de Idade";
+const resultado = idade >= 18 ? "Maior de idade" : "Menor de Idade";
 console.log(resultado);
+
+// IF abreviado
+if (idade === 18) 
+  console.log("Idade igual a 18");
+else
+  console.log("Não é igual a 18");
