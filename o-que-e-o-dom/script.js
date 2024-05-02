58713e1a4644a1c9b1cf2c839424876c20b85d78
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-02 17:24:06 -0400

    adicionar alert sem window

--- o-que-e-o-dom/script.js
+++ o-que-e-o-dom/script.js
@@ -1,5 +1,6 @@
 const titulo = document.querySelector("h1");
 window.alert("Hello");
+alert("Hello de novo")
 const href = window.location.href;
 console.log(document.body)
 if (href === "http://localhost:5500/o-que-e-o-dom/") {
