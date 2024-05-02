68aaa52b06af2e7e6f54efc2923b6b28052bf87e
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-02 18:50:01 -0400

    titulo.addEventListener click

--- o-que-e-o-dom/script.js
+++ o-que-e-o-dom/script.js
@@ -15,3 +15,7 @@ console.log(titulo.classList[1])
 console.log(titulo.innerText)
 console.log(titulo.id)
 console.log(titulo.offsetHeight)
+function clicou() {
+  titulo.innerText = "Clicou"
+}
+titulo.addEventListener("click", clicou)
