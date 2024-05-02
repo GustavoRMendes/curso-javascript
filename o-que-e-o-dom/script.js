c7bcb4c625051533eecebf1f28ad0c8c4a05d243
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-02 17:23:19 -0400

    adicionar document.body

--- o-que-e-o-dom/script.js
+++ o-que-e-o-dom/script.js
@@ -1,8 +1,7 @@
 const titulo = document.querySelector("h1");
-
 window.alert("Hello");
 const href = window.location.href;
-
+console.log(document.body)
 if (href === "http://localhost:5500/o-que-e-o-dom/") {
   console.log("true");
 } else {
