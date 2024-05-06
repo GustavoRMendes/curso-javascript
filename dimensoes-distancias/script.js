c59ff3a5a3fbb541807bf37956bbc14559292322
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 15:17:28 -0400

    matchMedia()

--- dimensoes-distancias/script.js
+++ dimensoes-distancias/script.js
@@ -23,3 +23,11 @@ console.log(
 if (window.innerWidth < 600) {
   console.log("Tela menor que 600px");
 }
+
+// matchMedia
+const small = window.matchMedia('("max-width: 600px")');
+if(small.matches) {
+  console.log("tela menor que 600px");
+} else {
+  console.log("tela maior que 600px")
+}
