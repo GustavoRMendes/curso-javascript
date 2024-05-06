57a63ff0c727b0b7c7dece09c373f962c883cbe9
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 10:52:28 -0400

    adicionar getAttribute, hasAttribute, setAttribute e removeAttribute

--- classes-atributos/script.js
+++ classes-atributos/script.js
@@ -14,3 +14,10 @@ menu.className += " vermelho"
 const animais = document.querySelector(".animais");
 console.log(animais.attributes);
 console.log(animais.attributes['data-texto']);
+
+//getAttribute e setAttribute
+const img = document.querySelector("img");
+console.log(img.getAttribute("src"));
+console.log(img.getAttribute("alt"));
+img.setAttribute("alt","Texto alternativo");
+console.log(img.hasAttribute("alt")); //Verifica - true
