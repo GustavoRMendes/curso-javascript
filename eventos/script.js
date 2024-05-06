597b18dca8797d4f23fcd8172854e8e0ad89f07b
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 16:20:14 -0400

    adicionar event no parâmetro

--- eventos/script.js
+++ eventos/script.js
@@ -1,9 +1,8 @@
 const img = document.querySelector("img");
-
 // img.addEventListener("click", () => {
 //   console.log("clicou");
 // })
-function callback() {
-  console.log("clicou");
+function callback(event) {
+  console.log(event);
 }
 img.addEventListener("click", callback);
