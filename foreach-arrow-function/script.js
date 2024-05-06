d94a992d4933e5c4f3c0efe35b9d013ea61845a7
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 10:15:13 -0400

    simplificar e encurtar a arrow function de apenas uma linha de comando

--- foreach-arrow-function/script.js
+++ foreach-arrow-function/script.js
@@ -14,8 +14,10 @@ titulosArray.forEach(function (item, index, array) {
 
 const imgs = document.querySelectorAll("img");
 
-imgs.forEach( (item, index, array) => {
-  console.log(item);
-  console.log(index);
-  console.log(array);
-});
+// imgs.forEach( (item, index, array) => {
+//   console.log(item);
+//   console.log(index);
+//   console.log(array);
+// });
+
+imgs.forEach((item) => console.log(item));
