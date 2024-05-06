edd1bd3b03d8177b7cfe46def413eccef1d0057f
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 10:09:13 -0400

    transformar HTMLCollection em array com Array.from()

--- foreach-arrow-function/script.js
+++ foreach-arrow-function/script.js
@@ -5,3 +5,9 @@ imgs.forEach(function (item,index,array) {
   // console.log(index);
   // console.log(array);
 })
+
+const titulos = document.getElementsByClassName('titulo');
+titulosArray = Array.from(titulos);
+titulosArray.forEach(function (item,index,array){
+  console.log(item,index,array);
+})
