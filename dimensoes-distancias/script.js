40a11ba38d3850773bddac7c6ea6d0c9170d1cca
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 14:59:18 -0400

    largura e altura

---
+++ dimensoes-distancias/script.js
@@ -0,0 +1,5 @@
+const listaAnimais = document.querySelector(".animais-lista");
+// scrollHeight, scrollWidth, clientHeight, clientWidth, offsetHeight, offsetWidth
+const height = listaAnimais.scrollHeight;
+console.log(listaAnimais.offsetWidth);
+console.log(height);
