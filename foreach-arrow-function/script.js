f8375d1a5b6ddbbcb42b3079f5d3c3f4a6df1a95
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 10:11:49 -0400

    modificar function para arrow Function

--- foreach-arrow-function/script.js
+++ foreach-arrow-function/script.js
@@ -1,13 +1,21 @@
+// const imgs = document.querySelectorAll("img");
+
+// imgs.forEach(function (item, index, array) {
+//   // console.log(item);
+//   // console.log(index);
+//   // console.log(array);
+// });
+
+const titulos = document.getElementsByClassName("titulo");
+titulosArray = Array.from(titulos);
+titulosArray.forEach(function (item, index, array) {
+  console.log(item, index, array);
+});
+
 const imgs = document.querySelectorAll("img");
 
-imgs.forEach(function (item,index,array) {
-  // console.log(item);
-  // console.log(index);
-  // console.log(array);
-})
-
-const titulos = document.getElementsByClassName('titulo');
-titulosArray = Array.from(titulos);
-titulosArray.forEach(function (item,index,array){
-  console.log(item,index,array);
-})
+imgs.forEach( (item, index, array) => {
+  console.log(item);
+  console.log(index);
+  console.log(array);
+});
