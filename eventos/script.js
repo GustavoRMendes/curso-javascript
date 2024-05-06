e65e44b6f64a0a52ccbe483619eeea7e45501a04
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-06 16:23:20 -0400

    adicionar propriedades do event

--- eventos/script.js
+++ eventos/script.js
@@ -6,3 +6,11 @@ function callback(event) {
   console.log(event);
 }
 img.addEventListener("click", callback);
+const animaisLista = document.querySelector(".animais-lista");
+function callbackLista(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
+  console.log(event.type);
+  console.log(event.path);
+}
+animaisLista.addEventListener("click", callbackLista);
