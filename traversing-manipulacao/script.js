9cfe3bc10e8704d2141ec879d65f8b4f18450170
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-07 11:41:18 -0400

    adicionar innerHTML e innerText nos elementos

--- traversing-manipulacao/script.js
+++ traversing-manipulacao/script.js
@@ -1,3 +1,13 @@
-// outerHTML === todo o html do elemento
-// innerHTML === html interno
+// outerHTML === todo o html do elemento.
+// innerHTML === html interno.
 // innerText === texto, sem tags.
+
+const h1 = document.querySelector("h1");
+const animaisLista = document.querySelector(".animais-lista");
+const animaisDescricao = document.querySelector(".animais-descricao");
+console.log(animaisDescricao.innerText);
+console.log(animaisDescricao.innerHTML);
+console.log(animaisLista.innerText); // empty <vazio>, porque são imagens.
+console.log(h1.innerHTML);
+
+h1.innerHTML = "Olá, Gustavo!";
