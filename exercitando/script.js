a136a8a3d2eed12c0c1f16ab985e571521f5837c
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-09 11:36:55 -0400

    add event.target img

--- exercitando/script.js
+++ exercitando/script.js
@@ -9,10 +9,15 @@ window.addEventListener("keydown", handleClick);
 
 function darkTheme(event) {
   if (event.key === "t") {
-    document.documentElement.classList.toggle("dark")
+    document.documentElement.classList.toggle("dark");
   }
 }
 
 window.addEventListener("keydown", darkTheme);
 
 // currentTarget  e Target
+const img = document.querySelector("img");
+function handleImg(event) {
+  console.log(event.target)
+}
+img.addEventListener("click", handleImg);
