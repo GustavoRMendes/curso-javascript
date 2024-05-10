148f7296804af2d6ae692ecdd216fc29eb2cf403
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-10 16:13:32 -0400

    adicionar javascript no site

--- navegacao-tabs/script.js
+++ navegacao-tabs/script.js
@@ -1,1 +1,19 @@
 // Navegação por Tab
+
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+  function activeTab(index) {
+    tabContent.forEach((tab) => {
+      tab.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
