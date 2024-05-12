1bbb025ba3eb9be5246fa40892e0e8351c0df304
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-12 16:23:51 -0400

    condicionar lista

--- exercitando/teste.js
+++ exercitando/teste.js
@@ -1,10 +1,12 @@
 const dt = document.querySelectorAll(".pergunta");
-dt[0].classList.add("ativo");
-dt[0].nextElementSibling.classList.add("ativo");
-function handleDt() {
-  this.classList.toggle("ativo");
-  this.nextElementSibling.classList.toggle("ativo");
+if (dt.length) {
+  dt[0].classList.add("ativo");
+  dt[0].nextElementSibling.classList.add("ativo");
+  function handleDt() {
+    this.classList.toggle("ativo");
+    this.nextElementSibling.classList.toggle("ativo");
+  }
+  dt.forEach((item) => {
+    item.addEventListener("click", handleDt);
+  });
 }
-dt.forEach((item) => {
-  item.addEventListener("click", handleDt);
-});
