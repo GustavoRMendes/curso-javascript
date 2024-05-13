916c9e7d4f3a91d1d84bcf89f51ee21cf907b2d6
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-13 15:32:21 -0400

    scrollIntoView()

--- navegacao-tabs/script.js
+++ navegacao-tabs/script.js
@@ -21,3 +21,17 @@ if (tabMenu.length && tabContent.length) {
 initNavTab();
 
 // noscript
+
+const linksInternos = document.querySelectorAll("a[href^='#']");
+function handleLink(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block:"start"
+  })
+}
+linksInternos.forEach((link) => {
+  link.addEventListener("click", handleLink)  
+}) 
