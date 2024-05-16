be292161e1b4aa002b55133bc886517f096005d8
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-15 21:46:53 -0400

    adicionar exercício 2

--- exercicios/index.js
+++ exercicios/index.js
@@ -1,2 +1,9 @@
 // Faça com que apareça Olá, Mundo! no console
-console.log("Olá, Mundo!")
+console.log("Olá, Mundo!");
+
+//Crie uma página HTML com um botão e faça com que ele exiba um alerta quando clicado.
+const buttonAlert = document.querySelector(".showAlert");
+function showMessage() {
+  alert("Alerta!");
+}
+buttonAlert.addEventListener("click", showMessage);
