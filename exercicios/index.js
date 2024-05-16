63c593880002607c5211b04ca7fd7b2862752834
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-15 21:50:07 -0400

    adicionar exercício 4

--- exercicios/index.js
+++ exercicios/index.js
@@ -9,5 +9,10 @@ function showMessage() {
 buttonAlert.addEventListener("click", showMessage);
 
 // Crie um programa que peça ao usuário um nome e depois exiba uma mensagem de saudação.
-const nome = prompt("Qual seu nome?")
+const nome = prompt("Qual seu nome?");
 const saudacao = alert("Olá " + nome + " !");
+
+// Crie uma página HTML com diferentes elementos e manipule-os usando JavaScript. Por exemplo, altere o texto de um parágrafo, adicione uma classe a um elemento ou esconda um elemento.
+const titulo = document.querySelector(".title");
+titulo.classList.add("ativo");
+titulo.innerHTML = "Novo Título";
