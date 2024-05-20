7782ff3ec10a1c3131bbf5232799ec9a442e8fb4
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-20 15:50:02 -0400

    escapsulando tudo na funcao de iniciar a animacao ao scroll

--- animacao-ao-scroll/script.js
+++ animacao-ao-scroll/script.js
@@ -49,21 +49,24 @@ function initLink() {
   });
 }
 initLink();
-
-const sections = document.querySelectorAll(".js-scroll");
-if (sections.length) {
-  const metadeWindow = window.innerHeight * 0.6; // 60%
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - metadeWindow < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else {
-        section.classList.remove("ativo");
-      }
-    });
+function initAnimamcaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const metadeWindow = window.innerHeight * 0.6; // 60%
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - metadeWindow < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
   }
-  animaScroll();
-  window.addEventListener("scroll", animaScroll);
 }
+
+initAnimamcaoScroll();
