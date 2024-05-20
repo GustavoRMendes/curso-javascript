658f7c3562e006a6efb709d4960fedf1bf69b6b0
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-20 15:43:15 -0400

    animar scroll da pagina

--- animacao-ao-scroll/script.js
+++ animacao-ao-scroll/script.js
@@ -49,3 +49,14 @@ function initLink() {
   });
 }
 initLink();
+
+const sections = document.querySelectorAll(".js-scroll");
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < 0) {
+      section.classList.add("ativo")
+    }
+  })
+}
+window.addEventListener("scroll", animaScroll)
