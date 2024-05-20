f66205da33ee3fa854661c09355e9f8c080698c4
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-19 21:44:10 -0400

    exercicio lista

--- exercicios/main.js
+++ exercicios/main.js
@@ -1,3 +1,8 @@
-const anoNascimento = Number(prompt("Insira o ano em que você nasceu: "))
-const idade = 2024 - anoNascimento
-console.log("No ano de 2024, você tem " + idade + " anos.")
+// const anoNascimento = Number(prompt("Insira o ano em que você nasceu: "))
+// const idade = 2024 - anoNascimento
+// console.log("No ano de 2024, você tem " + idade + " anos.")
+
+const lista = ['Gustavo','Felipe']
+for(let i = 0; i < lista.length; i++) {
+  console.log(lista[i])
+}
