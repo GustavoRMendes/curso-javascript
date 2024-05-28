227bc7b1390b720a01bee0918dcb279991c59a57
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-28 18:21:25 -0400

    Add function prototype

---
+++ prototype/main.js
@@ -0,0 +1,7 @@
+// Prototype é um objeto adicionado a uma função quando criada.
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+
+const gustavo = new Pessoa("Gustavo", 19);
