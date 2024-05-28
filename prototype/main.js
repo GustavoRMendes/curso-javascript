42a40b55a17d500db793c3beb46ac21f1fd7a7fb
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-28 18:44:15 -0400

    Add functions and methods

--- prototype/main.js
+++ prototype/main.js
@@ -2,6 +2,18 @@
 function Pessoa(nome, idade) {
   this.nome = nome;
   this.idade = idade;
+  this.andar = function () {
+    return "Andou pela propriedade do objeto";
+  };
 }
 
 const gustavo = new Pessoa("Gustavo", 19);
+
+// Adicionar propriedade ou método no objeto prototype da função.
+// Primeiro ele procura se existe o método ou propriedade no objeto.
+Pessoa.prototype.andar = function () {
+  return "andou";
+};
+Pessoa.prototype.respirar = function () {
+  return this.nome + " respirou";
+};
