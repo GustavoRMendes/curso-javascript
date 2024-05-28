dd1632bcc5fea261c9f66433faed44b520a18fe8
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-28 11:41:55 -0400

    comment object Dom

---
+++ constructor2/main.js
@@ -0,0 +1,9 @@
+// const Dom = {
+//   tagSeletor: "li",
+//   element() {
+//     return (tagSelecionada = document.querySelector(this.tagSeletor));
+//   },
+//   classeAtivar() {
+//     this.element().classList.add("ativo");
+//   },
+// };
