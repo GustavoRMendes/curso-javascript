2f3b41424ef9d3904256e068017fddab930d1370
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-02 22:06:21 -0400

    Add sub, div, and mul

--- exercicios/part1.js
+++ exercicios/part1.js
@@ -2,4 +2,10 @@ const numero1 = 10;
 const numero2 = 20;
 
 const soma = numero1 + numero2;
+const subtracao = numero1 - numero2;
+const multiplicacao = numero1 * numero2;
+const divisao = numero1 / numero2;
 console.log("A soma resulta em " + soma);
+console.log("A subtração resulta em " + subtracao);
+console.log("A multiplicação resulta em " + multiplicacao);
+console.log("A divisão resulta em " + divisao);
