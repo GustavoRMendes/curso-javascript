f9d5b25e5c1f7894575bd90f234bd6f31c3930c6
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-03 10:29:12 -0400

    String.concat()

--- string/main.js
+++ string/main.js
@@ -4,3 +4,8 @@ console.log(nome.length);
 
 // chartAt()
 console.log(nome.charAt(0)); //G
+
+// concat()
+const linguagem = "Javascript";
+const frase = " é a melhor linguagem do mundo!";
+console.log(linguagem.concat(frase));
