dca86ab6fc9f1d71d1005d621f32854aaf86f8af
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-02 22:13:26 -0400

    Odd and even

--- exercicios/part1.js
+++ exercicios/part1.js
@@ -30,3 +30,10 @@ function verificacao(numero) {
 }
 verificacao(20);
 // ----------------------------
+function parOuImpar(numero) {
+  if (numero % 2 === 0) {
+    console.log("Par");
+  } else {
+    console.log("Impar");
+  }
+}
