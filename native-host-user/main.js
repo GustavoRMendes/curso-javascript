90e7d2f661443dc7eec41d1fe00d1795bd7fd244
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-03 09:30:20 -0400

    Add exercise host, native and User

--- native-host-user/main.js
+++ native-host-user/main.js
@@ -11,3 +11,26 @@ const person = {
 };
 
 // Exercise
+
+// Liste 5 objetos nativos
+String;
+Array;
+Object;
+Function;
+Number;
+Boolean;
+// Liste 5 objetos do browser
+NodeList;
+HTMLCollection;
+Element;
+Window;
+Document;
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof navigator.getGamepads === "undefined") {
+  console.log("Yes");
+}
+if (typeof speechSynthesis.getVoices === "undefined") {
+  console.log("Yes");
+}
