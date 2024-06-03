65e152cca35b3dccf44b5ded49c51d6cb43e3283
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-02 22:07:53 -0400

    Add age

--- exercicios/part1.js
+++ exercicios/part1.js
@@ -9,3 +9,8 @@ console.log("A soma resulta em " + soma);
 console.log("A subtração resulta em " + subtracao);
 console.log("A multiplicação resulta em " + multiplicacao);
 console.log("A divisão resulta em " + divisao);
+
+// -------------------------------
+const idade = 18;
+const resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
+console.log(resultado);
