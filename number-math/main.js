2bae6bcdcf044c43a2e23fcbfbe8069ec0e9a362
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-04 10:52:40 -0400

    Add number.parseFloat()

--- number-math/main.js
+++ number-math/main.js
@@ -1,8 +1,11 @@
 // Number
 const idade = 19;
-
+const numero = 32.392;
 // isNaN
 console.log(Number.isNaN(idade)); // false
 
 // isInteger
 console.log(Number.isInteger(idade)); // true
+
+// parseFloat
+console.log(Number.parseFloat(numero));
