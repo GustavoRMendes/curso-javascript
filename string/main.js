761fd7435496a914b02d44f9f60dbe1ee5bc5168
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-04 09:22:39 -0400

    Add exercise about string

--- string/main.js
+++ string/main.js
@@ -62,3 +62,77 @@ const espacos = "    Oi tudo bom?           ";
 console.log(espacos.trim());
 console.log(espacos.trimStart());
 console.log(espacos.trimEnd());
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes1 = [
+  {
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
+  },
+  {
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
+  },
+  {
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
+  },
+];
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes1.forEach((transacao) => {
+  const numeroLimpo = +transacao.valor.replace("R$ ", "");
+  if (transacao.descricao.slice(0, 4) === "Taxa") {
+    taxaTotal += numeroLimpo;
+  } else {
+    recebimentoTotal += numeroLimpo;
+  }
+  console.log(taxaTotal);
+  console.log(recebimentoTotal);
+});
+
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+console.log(transportes.split(";"));
+
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+const htmlA = html.replaceAll("span", "a");
+console.log(htmlA);
+
+// Retorne o último caracter da frase
+const frase2 = "Melhor do ano!";
+console.log(frase2.charAt(frase2.length - 1));
+
+// Retorne o total de taxas
+const transacoes2 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
+let taxasTotal = 0;
+transacoes2.forEach((transacao) => {
+  transacao = transacao.toLocaleLowerCase();
+  transacao = transacao.trim();
+  transacao = transacao.slice(0, 4);
+  if (transacao === "taxa") {
+    taxasTotal++;
+  }
+  console.log(taxasTotal);
+});
