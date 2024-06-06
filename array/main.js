d15c56c3676953b90abbe75f901d8183a3788520
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-05 20:05:16 -0400

    Add new Array

--- array/main.js
+++ array/main.js
@@ -1,7 +1,7 @@
 // Array
 const lista = ["Gustavo", "Felipe", "Téo"];
 const precos = [10, 20, 30, 40];
-
+const carros = new Array("Corola", "Mustang", "Honda");
 console.log(lista[0]);
 console.log(lista[1]);
 console.log(lista[2]);
