ebb2c423f489da090ad18a5f47d06a78b3837fb2
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-10 23:36:52 -0400

    Format document

--- exercicios/main.js
+++ exercicios/main.js
@@ -1,11 +1,11 @@
-// // const anoNascimento = Number(prompt("Insira o ano em que você nasceu: "))
-// // const idade = 2024 - anoNascimento
-// // console.log("No ano de 2024, você tem " + idade + " anos.")
+// const anoNascimento = Number(prompt("Insira o ano em que você nasceu: "))
+// const idade = 2024 - anoNascimento
+// console.log("No ano de 2024, você tem " + idade + " anos.")
 
-// const lista = ['Gustavo','Felipe']
-// for(let i = 0; i < lista.length; i++) {
-//   console.log(lista[i])
-// }
+const lista = ["Gustavo", "Felipe"];
+for (let i = 0; i < lista.length; i++) {
+  console.log(lista[i]);
+}
 
-// const mensagem = "Olá"
-// console.log(mensagem)
+const mensagem = "Olá";
+console.log(mensagem);
