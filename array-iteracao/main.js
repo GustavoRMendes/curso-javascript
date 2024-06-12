81e91a1ed414c951fdb5ed05e90fb8d847c2f92f
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-12 18:59:35 -0400

    map with objects

--- array-iteracao/main.js
+++ array-iteracao/main.js
@@ -23,3 +23,22 @@ const dobroDosNumeros = numeros.map((num) => {
   return num * 2;
 });
 console.log(dobroDosNumeros);
+
+const aulas = [
+  {
+    nome: "HTML 1",
+    min: 15,
+  },
+  {
+    nome: "HTML 2",
+    min: 10,
+  },
+  {
+    nome: "CSS 1",
+    min: 20,
+  },
+  {
+    nome: "JS 1",
+    min: 25,
+  },
+];
