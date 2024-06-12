88cba2e5372aa331f249a822694338e66fc672eb
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-12 18:32:12 -0400

    Add forEach() with function

--- array-iteracao/main.js
+++ array-iteracao/main.js
@@ -4,3 +4,7 @@ const animais = ["Cavalo", "Lagartixa", "Boi"];
 animais.forEach((item, index, array) => {
   console.log(item.toUpperCase());
 });
+
+animais.forEach(function (item) {
+  console.log(item.toLowerCase());
+});
