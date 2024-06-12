c70f8ad3f3175d9aacfd3a79d4105200f45cfbc9
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-12 18:47:49 -0400

    Array.map()

--- array-iteracao/main.js
+++ array-iteracao/main.js
@@ -14,3 +14,7 @@ const li = document.querySelectorAll("li");
 li.forEach((item, index) => item.classList.add("ativo" + index));
 
 // map(callback(itemAtual,index,array))
+const mapRetornaArray = animais.map((item, index, array) => {
+  return item.toUpperCase() + " - " + index;
+});
+console.log(mapRetornaArray);
