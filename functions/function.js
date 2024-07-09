f1fb328bd2a5ed99acc7ed761e4fc37f9419133f
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-09 09:10:12 -0400

    function.call()

--- functions/function.js
+++ functions/function.js
@@ -6,3 +6,17 @@ function lerNome(nome) {
 console.log(lerNome("Gustavo"));
 console.log(lerNome.length);
 console.log(lerNome.name);
+
+// call
+const pessoa = {
+  nome: "Gustavo",
+  sobrenome: "Mendes",
+};
+function descricaoPessoa() {
+  console.log(this.nome + " " + this.sobrenome);
+}
+descricaoPessoa();
+descricaoPessoa.call(pessoa);
+//  apply
+
+// bind
