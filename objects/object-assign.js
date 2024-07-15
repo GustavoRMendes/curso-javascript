6d4dac91ddb81b29537e65a7e78d506ccdb01b2f
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-15 10:47:47 -0400

    Copy object

--- objects/object-assign.js
+++ objects/object-assign.js
@@ -17,3 +17,21 @@ const carro = {
   automovel: "Carro",
 };
 Object.assign(carro, funcoesDeAutomovel).acelerar();
+
+const programador = {
+  nome: this.nome,
+  idade: this.idade,
+  programador: this.programador,
+};
+
+const felipe = Object.assign({}, programador);
+felipe.nome = "Felipe";
+felipe.idade = 19;
+felipe.programador = true;
+console.log(felipe);
+
+const gustavo = Object.assign({}, programador);
+gustavo.nome = "Gustavo";
+gustavo.idade = 20;
+gustavo.programador = false;
+console.log(gustavo);
