e9993bd100e1163a46e01180cfd00d201d74df25
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-15 11:07:41 -0400

    Add object.defineProperties

--- objects/object-define-properties.js
+++ objects/object-define-properties.js
@@ -1,1 +1,34 @@
-// Object.defineProperties(alvo, propriedades)
+// Object.defineProperties(objeto, propriedades)
+
+// configurable: impede deletar e mudar valor
+// enumerable: torna ou não inumerável
+// value: valor da propriedade
+// writable: impede mudança de valor
+const carro = {};
+Object.defineProperties(carro, {
+  rodas: {
+    value: 4,
+    configurable: false,
+    writable: false,
+  },
+});
+carro.rodas = 3;
+delete carro.rodas;
+console.log(carro);
+
+// get()
+// set()
+const moto = {};
+Object.defineProperties(moto, {
+  rodas: {
+    get() {
+      return this._rodas;
+    },
+    set(valor) {
+      this._rodas = valor + " Rodas";
+    },
+  },
+});
+console.log(moto);
+moto.rodas = 2;
+console.log(moto);
